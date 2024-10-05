8036a1630bc4bd64ebd438eb32a978ae84b24253
Juan D Bertel <berteljuan77@gmail.com>
2024-10-05 11:56:54 -0500

    Core y Shared creados

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  Firebase: {
+    apiKey: "AIzaSyBh6lK97kCHnK3TAOWfHP_KcTyaBfCDr_8",
+    authDomain: "taller-ionic-ccdcf.firebaseapp.com",
+    projectId: "taller-ionic-ccdcf",
+    storageBucket: "taller-ionic-ccdcf.appspot.com",
+    messagingSenderId: "137020009960",
+    appId: "1:137020009960:web:c08674e301a297ac7e485d",
+    measurementId: "G-L023EGDBGM"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  Firebase: {
+    apiKey: "AIzaSyBh6lK97kCHnK3TAOWfHP_KcTyaBfCDr_8",
+    authDomain: "taller-ionic-ccdcf.firebaseapp.com",
+    projectId: "taller-ionic-ccdcf",
+    storageBucket: "taller-ionic-ccdcf.appspot.com",
+    messagingSenderId: "137020009960",
+    appId: "1:137020009960:web:c08674e301a297ac7e485d",
+    measurementId: "G-L023EGDBGM"
+  }
 };
 
 /*
