f44c9a61768c7d16542ae2ac5d41d8a6fb898bb8
Juan D Bertel <berteljuan77@gmail.com>
2024-10-05 12:25:21 -0500

    O.o

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { InputComponent } from './components/input/input.component';
+import { FormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
 
-
+const Componets = [InputComponent];
+const Modules = [
+  CommonModule,
+  FormsModule,
+  IonicModule
+];
 
 @NgModule({
-  declarations: [],
+  declarations: [... Componets],
   imports: [
-    CommonModule
-  ]
+    ... Modules
+  ],
+  exports: [... Componets, ... Modules]
 })
 export class SharedModule { }
