e7d4d07071129d80a0cc986e792a122e6b58837e
Edward <edward11.eagm@gmail.com>
2024-10-06 15:03:22 -0500

    Vista formulario de tarea listo

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
 
 import { HomePageRoutingModule } from './home-routing.module';
@@ -10,6 +10,8 @@ import { SharedModule } from 'src/app/shared/shared.module';
 
 @NgModule({
   imports: [
+    IonicModule,
+    ReactiveFormsModule,
     HomePageRoutingModule,
     SharedModule
   ],
--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms'; // Importaciones correctas
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,27 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  taskForm: FormGroup;  // Definir el FormGroup
+  minDate: string;
 
-  constructor() {}
+  constructor(private formBuilder: FormBuilder) {
+    const today = new Date();
+    this.minDate = today.toISOString().split('T')[0];  // Inicializar la fecha mínima
 
+    // Inicializar el formulario con validaciones
+    this.taskForm = this.formBuilder.group({
+      title: ['', Validators.required],
+      description: ['', Validators.required],
+      date: ['', Validators.required],
+      userId: ['', Validators.required],
+      done: [false]  // Valor por defecto false para el campo done
+    });
+  }
+
+  onSubmit() {
+    if (this.taskForm.valid) {
+      const taskData = this.taskForm.value;
+      console.log('Task data:', taskData);  // Aquí puedes manejar la información de la tarea (enviar a backend, etc.)
+    }
+  }
 }
