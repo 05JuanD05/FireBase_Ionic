9800f9f2e29aef0287fbe83e0d3b07a0dc207ed2
Edward <edward11.eagm@gmail.com>
2024-10-06 16:39:06 -0500

    item de actualizar y boton logaut listo

--- src/app/list/list.page.ts
+++ src/app/list/list.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-list',
@@ -7,7 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListPage implements OnInit {
 
-  constructor() { }
+  constructor(private navCtrl: NavController) { }
+  
+  goToUpdate(){
+    this.navCtrl.navigateForward('/Update');
+  }
+
+  logout() {
+    this.navCtrl.navigateRoot('/login');
+  }
 
   ngOnInit() {
   }
