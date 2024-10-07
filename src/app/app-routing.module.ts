1e6833f6bcb1253bed8bddbf674035a59d9301df
Luis Garavito Hernandez <luisdagh11@gmail.com>
2024-10-07 12:50:36 -0500

    login y register actualizados

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
-
+import { authGuard } from './shared/guard/auth.guard';
 const routes: Routes = [
   {
     path: 'home',
     loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule),
-    //canActivate:[authGuard]
+    canActivate:[authGuard]
   },
 
   {
