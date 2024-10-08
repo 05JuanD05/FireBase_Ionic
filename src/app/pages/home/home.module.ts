8950b7f9d82e5b3c54956504c788afa3ac3563bc
JesusPalacio <jesusdanielpalacioavila50@gmail.com>
2024-10-08 11:38:12 -0500

    Diseño login listo

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -3,8 +3,6 @@ import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
-import { IonicModule } from '@ionic/angular';
-import { ReactiveFormsModule } from '@angular/forms';
 
 import { SharedModule } from 'src/app/shared/shared.module';
 import { HomePageRoutingModule } from './home-routing.module';
